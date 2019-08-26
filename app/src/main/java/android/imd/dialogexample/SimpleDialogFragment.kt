2fc6d10d7a2b7abf5a634db0c33c85026dd4ea16
Victor <victor_brandao@outlook.com>
2019-08-26 19:59:52 -0300

    Simple Dialog added

---
+++ app/src/main/java/android/imd/dialogexample/SimpleDialogFragment.kt
@@ -0,0 +1,26 @@
+package android.imd.dialogexample
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class SimpleDialogFragment: DialogFragment(), DialogInterface.OnClickListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(requireActivity())
+            .setTitle("Escolha uma linguagem")
+            .setItems(R.array.linguagens, this)
+            .create()
+    }
+
+    override fun onClick(p0: DialogInterface?, p1: Int) {
+        val linguagens = requireActivity()
+            .resources.getStringArray(R.array.linguagens)
+        val linguagem = linguagens[p1]
+
+        Toast.makeText(requireActivity(), linguagem, Toast.LENGTH_SHORT).show()
+    }
+}
